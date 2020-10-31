313636b74bce517723bd7c04367f025fb46bd0e3
Iancu Vlad <vladi@stonesouptech.com>
2020-11-01 01:01:19 +0200

    Removed credentials file

--- MySqlConnect.php
+++ MySqlConnect.php
@@ -1,5 +1,6 @@
 <?php 
+require_once "./Credentials.php";
 function getMysqliConnection() {
-    return mysqli_connect(gethostbyname(gethostname()), "local", "MySql2020-Remote-Password#", "hotel_db" );
+    return mysqli_connect(gethostbyname(gethostname()), MYSQL_USER, MYSQL_PASSWORD, MYSQL_SCHEMA);
 }
 ?>
