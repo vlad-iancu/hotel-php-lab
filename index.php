12f2dd345415227d418793f773d204ddec168981
Iancu Vlad <vladi@stonesouptech.com>
2020-11-25 17:23:06 +0200

    Preflight

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     require_once './api/hotel/identity/HotelIdentityRoutes.php';
     require_once './api/rooms/HotelRoomRoutes.php';
     require_once './Router.php';
+    if($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+        error_log("Front controller intercepted CORS preflight");
+    }
     $router = new Router();
     $router->authorize = 'authorize';
     addAuthenticationRoutes($router);
